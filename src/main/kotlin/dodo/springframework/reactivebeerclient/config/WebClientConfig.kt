18af3182bfd01b5512b715b44b2e0160d63a8cfc
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2022-09-08 14:07:27 +0700

    😞webClient operation

---
+++ src/main/kotlin/dodo/springframework/reactivebeerclient/config/WebClientConfig.kt
@@ -0,0 +1,16 @@
+package dodo.springframework.reactivebeerclient.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientConfig {
+
+    @Bean
+    fun webClient(): WebClient {
+        return WebClient.builder()
+            .baseUrl(WebClientProperties().BASE_URL)
+            .build()
+    }
+}
