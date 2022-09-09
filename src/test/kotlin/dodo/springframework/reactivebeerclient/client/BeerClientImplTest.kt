03fc54722482e99cc141f0d67d2855fbe0507fae
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2022-09-09 09:47:47 +0700

    logger for test

--- src/test/kotlin/dodo/springframework/reactivebeerclient/client/BeerClientImplTest.kt
+++ src/test/kotlin/dodo/springframework/reactivebeerclient/client/BeerClientImplTest.kt
@@ -24,8 +24,6 @@ internal class BeerClientImplTest {
 
         assertThat(pagedList).isNotNull;
         assertThat(pagedList.content.size).isGreaterThan(0);
-        println()
-        println(pagedList.toString())
     }
 
     @Test
